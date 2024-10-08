f6e67bada060537fb761dd50c6d80ec18e0999f9
Giuliano Scaglioni <scaglioni96@gmail.com>
2024-10-08 19:43:28 -0300

    language corrections

--- src/components/country/CountryInfo.jsx
+++ src/components/country/CountryInfo.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
-import { Line } from 'react-chartjs-2';  // Importar el gráfico de línea
+import { Line } from 'react-chartjs-2'; // for graph
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -24,7 +24,7 @@ import {
   );
 
 const CountryInfo = () => {
-  const { countryCode } = useParams();  // Obtener el código del país desde la URL
+  const { countryCode } = useParams();  // Obtaining the country code from URL
   const [countryData, setCountryData] = useState(null);
   const [borderCountries, setBorderCountries] = useState([]);
   const [populationData, setPopulationData] = useState([]);
@@ -34,15 +34,15 @@ const CountryInfo = () => {
   useEffect(() => {
     const fetchCountryData = async () => {
       try {
-        // Obtener la información básica del país
+        // basic information from country
         const countryInfoResponse = await axios.get(`http://localhost:5000/country-info/${countryCode}`);
         setCountryData(countryInfoResponse.data);
 
         console.log(countryInfoResponse);
-        // Obtener los países limítrofes
+        // saving border countries
         setBorderCountries(countryInfoResponse.data.borders);
 
-        // Obtener los datos de población
+        // saving population data
         setPopulationData(countryInfoResponse.data.populationData)
 
 
@@ -62,11 +62,11 @@ const CountryInfo = () => {
 
   return (
     <div className='p-5 d-flex flex-column justify-content-between align-items-center'>
-      {/* Mostrar el nombre y la bandera del país */}
+      {/* Show name and flag */}
       <h1>{populationData.country}</h1>
       <img className='w-25' src={countryData.flagUrl} alt={`Flag of ${populationData.country}`} />
 
-      {/* Lista de países limítrofes */}
+      {/* border countries list */}
       <div>
         <h2>Border Countries</h2>
         <ul>
@@ -78,7 +78,7 @@ const CountryInfo = () => {
         </ul>
       </div>
 
-      {/* Gráfico de población */}
+      {/* population graph */}
       <div className='d-flex flex-column align-items-center'>
         <h2>Population Over Time</h2>
         <Line
--- src/components/country/CountryList.jsx
+++ src/components/country/CountryList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-//import axios from 'axios';
+
 import clientAxios from '../../config/axios';
 import { Link } from 'react-router-dom';
 
